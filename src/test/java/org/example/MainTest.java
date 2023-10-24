678537b24c49d66070d944c250f07d3d43872bcd
@kerbymart <kerbymart@gmail.com>
2023-10-24 20:37:12 +0800

    Fix Arquillian test and Shrinkwrap deployment issues
    This commit addresses multiple issues:
    - Corrected the dependencies for the Arquillian test.
    - Updated Shrinkwrap deployment using the Maven resolver, ensuring
      proper loading of packages, resources, and libraries.
    - Updated the test for the Restlet client to access and
      test from the dynamic deployment URL or context.
    
    These changes ensure better stability and accuracy in our testing environment.

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,19 +3,22 @@ package org.example;
 import org.example.resources.HelloWorldResource;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.restlet.Client;
-import org.restlet.Component;
 import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.data.Method;
 import org.restlet.data.Protocol;
 import org.restlet.data.Status;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -23,30 +26,14 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(Arquillian.class)
 public class MainTest {
 
-    private static Component component;
     private Client client;
+
+    @ArquillianResource
+    URL deploymentUrl;
 
     @Deployment
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, "test.war")
-                .addClass(Main.class)
-                .addClass(HelloWorldResource.class);
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-        // Start the embedded Restlet server
-        component = new Component();
-        component.getServers().add(Protocol.HTTP, 8080);
-        component.getDefaultHost().attach("/", new Main());
-        component.start();
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-        if (component != null) {
-            component.stop();
-        }
+        return createMavenDeployment();
     }
 
     @Before
@@ -62,14 +49,42 @@ public class MainTest {
     }
 
     @Test
-    public void testHelloWorldResource() throws IOException {
-        Request request = new Request(Method.GET, "http://localhost:8080/hello");
+    public void testRootResource() {
+        String targetUrl = deploymentUrl.toString();
+
+        Request request = new Request(Method.GET, targetUrl);
         Response response = client.handle(request);
 
         assertNotNull(response);
         assertEquals(Status.SUCCESS_OK, response.getStatus());
-        assertEquals("hello, world", response.getEntity().getText());
+        assertEquals("welcome to the root resource", response.getEntityAsText());
+    }
+
+    @Test
+    public void testHelloWorldResource() {
+        String targetUrl = deploymentUrl.toString() + "hello";
+
+        Request request = new Request(Method.GET, targetUrl);
+        Response response = client.handle(request);
+
+        assertNotNull(response);
+        assertEquals(Status.SUCCESS_OK, response.getStatus());
+        assertEquals("hello, world", response.getEntityAsText());
+    }
+
+    public static WebArchive createSimpleDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addClass(Main.class)
+                .addClass(HelloWorldResource.class);
+    }
+
+    public static WebArchive createMavenDeployment() {
+        File[] files = Maven.resolver().loadPomFromFile("pom.xml")
+                .importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addPackages(true, "org.example")
+                .addAsResource(new File("src/main/resources/"), "")
+                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/web.xml"), "web.xml")
+                .addAsLibraries(files);
     }
 }
-
-
